553ae0779dddcc14d0c3b9c779c37bfc54ed4ed1
mihastech <mihastechdeveloper@gmail.com>
2024-10-31 11:22:55 +0300

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
         {children}
       </body> */}
 <head>
-        <link rel="icon" href="/BaajiTechImage.jpg"/>
+        <link rel="icon" href="/business-finance-money-svgrepo-com.svg"/>
       </head>
 
 <body className={`${inter.className} min-h-screen flex flex-col px-8`}>{children}</body>
