679f1b20127fb8c985c39d7cf33f9008f2e61ac2
mihastech <mihastechdeveloper@gmail.com>
2024-10-31 09:08:26 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [{
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        port: ''
-      }]
-    }
-  };
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: '*.supabase.co',
+      port: ''
+    }]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard', // Change this to your desired page
+        permanent: true,
+      },
+    ]
+  }
+};
 
 export default nextConfig;
